be063c8477f5d5920fbdf58f5dc601a9f1e8b90b
mhaao@kth.se <mhaao@kth.se>
2023-06-03 00:59:49 +0200

    3

--- mail/contact.php
+++ mail/contact.php
@@ -9,17 +9,12 @@ $email = strip_tags(htmlspecialchars($_POST['email']));
 $m_subject = strip_tags(htmlspecialchars($_POST['subject']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
-$to = "alobaidi.murtadha@gmail.com"; //
+$to = "mimo@spangatradgard.se"; //
 $subject = "$m_subject:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
+$header = "From: $email";
+$header .= "Reply-To: $email";	
 
-// Additional headers
-$headers = 'MIME-Version: 1.0' . "\r\n";
-$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-$headers .= 'From: '.$name.' <'.$email.'>' . "\r\n" .
-    'Reply-To: '.$email. "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-
-if(!mail($to, $subject, $body, $headers))
+if(!mail($to, $subject, $body, $header))
   http_response_code(500);
 ?>
