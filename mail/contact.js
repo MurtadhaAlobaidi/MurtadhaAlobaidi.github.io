f0c65822eb3d5292fc6a447f0ae18a37ec5de24d
mhaao@kth.se <mhaao@kth.se>
2023-07-17 00:44:32 +0200

    Update contact.js

--- mail/contact.js
+++ mail/contact.js
@@ -1,28 +1,4 @@
 $(function () {
-    $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
-        preventSubmit: true,
-        submitError: function ($form, event, errors) {
-            // additional error messages or events
-        },
-        submitSuccess: function ($form, event) {
-            event.preventDefault(); // prevent default submit behavior
-            // get values from form
-            var name = $("input#name").val();
-            var email = $("input#email").val();
-            var subject = $("input#subject").val();
-            var message = $("textarea#message").val();
-            // check if any field is empty and prevent form submission
-            if (name === "" || email === "" || subject === "" || message === "") {
-                return;
-            }
-            var $this = $("#sendMessageButton");
-            $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
-        },
-        filter: function () {
-            return $(this).is(":visible");
-        },
-    });
-
     $("a[data-toggle=\"tab\"]").click(function (e) {
         e.preventDefault();
         $(this).tab("show");
