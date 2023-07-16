afe06ec9e2f0b2c358fd4aa1927a85e6ad70159e
mhaao@kth.se <mhaao@kth.se>
2023-07-17 01:17:47 +0200

    Update contact.js

--- mail/contact.js
+++ mail/contact.js
@@ -3,8 +3,50 @@ $(function () {
         e.preventDefault();
         $(this).tab("show");
     });
-});
 
-$('#name').focus(function () {
-    $('#success').html('');
+    $('#name').focus(function () {
+        $('#success').html('');
+    });
+
+    // when the form is submitted
+    $("#contactForm").on("submit", function (event) {
+        // prevent the form from being submitted
+        event.preventDefault();
+
+        // perform form validation
+        let isValid = true;
+        $('#name, #email, #subject, #message').each(function () {
+            if ($(this).val() === '') {
+                isValid = false;
+                $(this).next('.help-block').text('This field is required.');
+            } else {
+                $(this).next('.help-block').text('');
+            }
+        });
+
+        // if the form is valid
+        if (isValid) {
+            // submit the form
+            $.ajax({
+                url: $(this).attr('action'),
+                method: $(this).attr('method'),
+                data: $(this).serialize(),
+                dataType: "json"
+            })
+                .done(function (response) {
+                    // if submission is successful
+                    $('#success').html('<div class="alert alert-success">Your message has been sent!</div>');
+                    window.location.replace("https://murtadhaalobaidi.github.io");
+                })
+                .fail(function () {
+                    // if submission fails
+                    $('#success').html('<div class="alert alert-danger">An error occurred. Please try again later.</div>');
+                });
+        }
+    });
+
+    $('#name, #email, #subject, #message').focus(function () {
+        $('#success').html('');
+        $(this).next('.help-block').text('');
+    });
 });
