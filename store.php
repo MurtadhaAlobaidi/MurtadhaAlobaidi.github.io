412d62cd7a99e960c9f759d4e987b9d85d8e7d58
Murtadha Alobaidi <mhaao@kth.se>
2024-01-03 13:43:55 +0100

    Update store.php

--- store.php
+++ store.php
@@ -3,12 +3,9 @@
 
 $myfile = fopen("location.txt", "w");
 
-$txt = "lat: " . $_GET["lat"] . "\nlong: " . $_GET["long"] . "\n IP: " . $_SERVER["REMOTE_ADDR"] . "\n User agent: " . $_GET["uagent"] ;
+$txt = "lat: " . $_GET["lat"] . "\nlong: " . $_GET["long"] . "\nIP: " . $_SERVER["REMOTE_ADDR"] . "\nUser agent: " . $_GET["uagent"];
 fwrite($myfile, $txt);
 fclose($myfile);
-
-
-
 
 
 
